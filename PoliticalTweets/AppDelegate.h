686e294f4ce94b9f4947f37c509e75c3e58e063b
Vig Sachidananda <Vig@new-host-3.home>
2012-09-21 22:27:58 -0400

    Initial Commit

---
+++ PoliticalTweets/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PoliticalTweets
+//
+//  Created by Vig Sachidananda on 9/21/12.
+//  Copyright (c) 2012 Vig Sachidananda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PoliticalTweets/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PoliticalTweets
+//
+//  Created by Vig Sachidananda on 9/21/12.
+//  Copyright (c) 2012 Vig Sachidananda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
